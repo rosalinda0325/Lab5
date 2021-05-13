322d984ecb1d41f5796fefffe2b02fc7eb39ad51
rosalinda0325 <rosalinda03@mail.fresnostate.edu>
2021-05-13 15:03:36 -0700

    finished structured english

--- lab_5.py
+++ lab_5.py
@@ -15,60 +15,6 @@ key words and return back the value in the dictionary
 print out the translated sentence to user
 """
 
-"""
-
-main()
- dictionary =translate():
- translate()
-
-
-translate(sentence, dictionary)
- word = each word in sentence
- print translate()
-
-
-
-create_dictionay()
- read in textese.txt
- create list = each line from file
- close file
- create a dictionary off lsit
- return
-
-
-main()
-
-"""
-
-def main():
-    sentence = "enjoy the excellent band tonight"
-    dictionary = create_dictionary("textese.txt")
-    translate(sentence, dictionary)
-
-def create_dictionary(txt_file):
-    infile = open(txt_file, "r")
-    words = [word.rstrip() for word in infile]
-    infile.close()
-
-    print("words", words)
-    translation = {}
-    for word in words:
-        [k, v] = word.split(",")
-        translation[k] = v
-    return translation
-
-    return { 
-        "tonight" : "2nite", "late": "l8"
-    }
-
-
-def translate(sentence, dictionary):
-    print("From translate", sentence)
-    words = sentence.split() 
-    for word in words:
-        print(dictionary.get(word, word), " ", end="")
-
-main()
 
 
 
