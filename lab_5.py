556fa74972f5a88f4833a933a3e734f8f41347b2
rosalinda0325 <rosalinda03@mail.fresnostate.edu>
2021-05-13 15:06:45 -0700

    pseudocode

--- lab_5.py
+++ lab_5.py
@@ -15,6 +15,30 @@ key words and return back the value in the dictionary
 print out the translated sentence to user
 """
 
+"""
+
+main()
+ dictionary =translate():
+ translate()
+
+
+translate(sentence, dictionary)
+ word = each word in sentence
+ print translate()
+
+
+
+create_dictionay()
+ read in textese.txt
+ create list = each line from file
+ close file
+ create a dictionary off lsit
+ return
+
+
+main()
+
+"""
 
 
 
